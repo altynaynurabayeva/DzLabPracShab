31d25fd778f2d135b9b5051feba4b5385fc47c3a
altynaynurabayeva <altynay.nurabayeva@gmail.com>
2025-10-21 22:40:50 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+       Console.WriteLine("=== Template Method ===\n");
+        TemplateDemo.Run();
+
+        Console.WriteLine("\n=== Mediator ===\n");
+        MediatorDemo.Run();    }
+}
